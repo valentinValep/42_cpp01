ddb664e4328db1c96a464385ce4d59c057335f5b
Valentin LEPILLER <vlepille@student.42.fr>
2023-09-09 21:03:09 +0200

    Make ref and ptr syntaxe consistant

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,8 +4,8 @@
 int main(void)
 {
 	std::string str = "HI THIS IS BRAIN";
-	std::string* ptr = &str;
-	std::string& ref = str;
+	std::string *ptr = &str;
+	std::string &ref = str;
 
 	std::cout << &str << std::endl;
 	std::cout << ptr << std::endl;
